79d2f4eac5b3d43429468db3ed4db761e8880ad3
snixtho <snixtho@gmail.com>
2019-03-18 14:34:01 +0100

    added generic packet deserializing and tests

---
+++ MicoServer/Packets/Packet..cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicoServer.Packets {
+    public class Packet {
+        public PacketType Type;
+        public int DataSize;
+        public byte[] Data;
+
+        public Packet(byte[] data) {
+            if (data.Length < 5) { // byte + int = 5 bytes
+                throw new FormatException("Data must be 5 bytes or more.");
+            }
+
+            Type = (PacketType)data[0];
+            DataSize = data[1] | (data[2] << 8) | (data[3] << 16) | (data[4] << 24);
+            Data = new byte[DataSize];
+            try {
+                Array.Copy(data, 5, Data, 0, DataSize);
+            } catch (Exception) { }
+        }
+    }
+}
